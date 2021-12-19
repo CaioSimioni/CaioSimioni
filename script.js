7705a64a8ec3f4282c4c845a005487a6607b72bb
Yuri <caioribeirosimioni@hotmail.com>
2021-12-19 00:06:18 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function criarElement(tag, parent, conteudo, classe){
+
+    if(typeof(tag) == "string"){
+
+
+
+    }else{
+        return console.log("Erro ao criar um Elemento.  tipo da TAG é inválido.");
+    }
+
+    return null;
+}
+
+criarElement("w");
+
